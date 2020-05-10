8003c832643fcad8bbaa7ed8dea213346907578e
Dinakar Guniguntala <dinakar.g@in.ibm.com>
2020-05-10 19:43:01 +0530

    Send Vehicle service JSON object.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kabanero!");
+	let service = [];
+	service.push( 
+		{ 
+      customerInfo: { "name" : "MSIL", "vehicleNumber" : "KA04AA1234" },
+      // nextServiceDue: { date: "15 May 2020", KM: "38400" }, 
+		  vehicleHealth: { brakePad: "GOOD", oilLevel: "MEDIUM" }
+		});
+ 
+	let sdata = JSON.stringify(service, null, 2);
+	//console.log(sdata);
+	res.send(sdata);
 });
  
 module.exports.app = app;
