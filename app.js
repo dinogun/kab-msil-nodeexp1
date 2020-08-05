d72161da714d7a34a61ae2e6743a5a366a6c1485
Dinakar Guniguntala <dinakar.g@in.ibm.com>
2020-08-05 10:38:52 +0530

    Update JSON to add payment details

--- app.js
+++ app.js
@@ -5,37 +5,34 @@ app.get('/', (req, res) => {
 	
 	service.push(
 		{
-			[
-			  {
-				"username": "Rohan",
-				"primaryPlan": "prepaid",
-				"msg_limit": "56",
-				"call_limit": "1245 mins",
-				"data_limit": "55.6 GB",
-				"plans": [
-				  {
-					"type": "Due by 16th Aug 2020",
-					"amount": "450.50",
-					"name": "August 2020"
-				  },
-				  {
-					"type": "Paid on 18th Jul 2020",
-					"amount": "345",
-					"name": "July 2020"
-				  },
-				  //{
-			//		"type": "Paid on 18th Jul 2020",
-			//		"amount": "INR 250",
-			//		"name": "June 2020"
-			//	  },
-				  {
-					"type": "prepaid",
-					"amount": "INR 250",
-					"name": "June 2020"
-				  }
+			"username": "Rohan",
+			"primaryPlan": "prepaid",
+			"msg_limit": "56",
+			"call_limit": "1245 mins",
+			"data_limit": "55.6 GB",
+			"plans":
+				[
+					{
+						"type": "Due by 16th Aug 2020",
+						"amount": "450.50",
+						"name": "August 2020"
+				  	},
+					{
+						"type": "Paid on 18th Jul 2020",
+						"amount": "345",
+						"name": "July 2020"
+					},
+					//{
+					//		"type": "Paid on 18th Jul 2020",
+					//		"amount": "INR 250",
+					//		"name": "June 2020"
+					//},
+					{
+						"type": "prepaid",
+						"amount": "INR 250",
+						"name": "June 2020"
+				  	}
 				]
-			  }
-			]
 		}
 	);
 
